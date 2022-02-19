5aa09381b0ba89ddce7a4e39075c9131765fae63
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-02-19 13:25:37 +0100

    empezando este bikram

---
+++ index.js
@@ -0,0 +1,8 @@
+let variableSinValor
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = 2 * PI;
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
